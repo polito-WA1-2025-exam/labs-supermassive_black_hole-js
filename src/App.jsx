b17b3623929c727baf07ad6a6404dc0fc790f597
Amir Othmani <s344108@studenti.polito.it>
2025-05-14 20:50:31 +0200

    homepage is full-screen now

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import { Container } from 'react-bootstrap'
 
 function App() {
 
   return (
     <>
-     <Header/>
-     <p>Welcome to Surplus Food Website.</p>
-     <p>This is the user web-page.</p>
-     <p>We're still working on the website.</p>
-     <Footer/>
+      <Header/>
+      <Container fluid className='flex-grow-1'>
+        <p>Welcome to Surplus Food Website.</p>
+        <p>This is the user web-page.</p>
+        <p>We're still working on the website.</p>
+      </Container>
+      <Footer/>
     </>
   )
 }
