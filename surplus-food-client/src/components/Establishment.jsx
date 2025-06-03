0c6e1468e5e53f4b0c1abbf038dc1cc574df709b
Amir Othmani <s344108@studenti.polito.it>
2025-06-03 18:18:20 +0200

    first N establishments

--- surplus-food-client/src/components/Establishment.jsx
+++ surplus-food-client/src/components/Establishment.jsx
@@ -1,5 +1,4 @@
 import my_image from '../assets/gigachad.png'
-import my_image_2 from '../assets/random meme.jpg'
 import { Container, Row, Col, Card } from 'react-bootstrap';
 
 // The establishments data are passed as props here.
@@ -7,40 +6,22 @@ function ListEstablishments(props) {
 
     const establishments = props.establishments;
 
-    const half = Math.ceil(establishments.length / 2);
-    const row1 = establishments.slice(0, half);
-    const row2 = establishments.slice(half);
+    // take only the first N establishments to display
+    const firstNEstablishments = establishments.slice(0, 4);
       
     return (
-        <Container className="mt-4 flex-grow-1">
-            <Row className="mb-4">
-                {row1.map(card => (
-                    <Col key={card.id} md={4}>
-                        <Card>
-                        <Card.Img variant='top' src={my_image}
-                        style={{ width: '200px', height: '200px', objectFit: 'cover', margin: '0 auto', paddingTop: '10px' }}/>
-                        <Card.Body>
-                            <Card.Title>{card.name}</Card.Title>
-                            <Card.Text>{card.address}</Card.Text>
-                        </Card.Body>
-                        </Card>
-                    </Col>
-                ))}
-            </Row>
-            <Row>
-                {row2.map(card => (
-                    <Col key={card.id} md={4}>
-                        <Card>
-                        <Card.Img variant='top' src={my_image_2}
-                        style={{ width: '200px', height: '200px', objectFit: 'cover', margin: '0 auto', paddingTop: '10px' }}/>
-                        <Card.Body>
-                            <Card.Title>{card.name}</Card.Title>
-                            <Card.Text>{card.address}</Card.Text>
-                        </Card.Body>
-                        </Card>
-                    </Col>
-                ))}
-            </Row>
+        <Container className="mt-4 flex-grow-1 d-flex flex-row">
+            
+            {firstNEstablishments.map(card => (
+                <Card key={card.id} className='m-4' style={ {width: '200px'}}>
+                <Card.Img variant='top' src={my_image}
+                style={{ objectFit: 'cover', margin: '0 auto', paddingTop: '10px' }}/>
+                <Card.Body>
+                    <Card.Title>{card.name}</Card.Title>
+                    <Card.Text>{card.address}</Card.Text>
+                </Card.Body>
+                </Card>
+            ))}
         </Container>
     );
 }
